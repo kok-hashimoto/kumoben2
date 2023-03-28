b3cae12376e278ec41d1d993ac14b60b395dbe72
hsmtkk <hsmtkk@gmail.com>
2023-03-28 22:51:22 +0000

    add google app engine code

---
+++ google-app-engine/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	portStr := os.Getenv("PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		log.Fatalf("failed to parse %s as int; %s", portStr, err.Error())
+	}
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/person", personHandler)
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", port)))
+}
+
+type responseSchema struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func personHandler(c echo.Context) error {
+	idStr := c.QueryParam("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return fmt.Errorf("failed to parse %s as int; %w", idStr, err)
+	}
+	resp := responseSchema{ID: id}
+	switch id % 3 {
+	case 0:
+		resp.Name = "Alice"
+	case 1:
+		resp.Name = "Bravo"
+	default:
+		resp.Name = "Charlie"
+	}
+	return c.JSON(http.StatusOK, resp)
+}
